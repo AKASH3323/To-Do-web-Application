232b0a99e14f98ff967b0ee02289c878b7ae21ed
Akash S <akashsenguttuvan02@gmail.com>
2025-12-26 14:29:05 +0530

    changes commited

---
+++ src/pages/about.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+ export default function About(){
+    return(<h1>About Me</h1>)
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from "react";
+// import "./Components/about.css"
+// export default function About(){
+//     const[count,setCount]=useState(0)
+//     useEffect(()=>{
+// setCount(count+1)
+//     },[setCount])
+
+    
+
+//     return(<div>
+//         <h1>Count:{count}</h1>
+//         <h1>I’m a passionate Front-End Developer dedicated to crafting clean, responsive, and user-friendly web experiences. With a strong foundation in HTML, CSS, and JavaScript, I specialize in turning ideas into interactive designs that not only look great but also perform seamlessly across devices.
+// </h1>
+//     </div>)
+// }
---
+++ src/pages/contact.jsx
@@ -0,0 +1,33 @@
+import react from "react"
+ export default function Contact(params) {
+    return(<h1>Contact me</h1>)
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+
+// export default function Contact(){
+//     return(
+//         <h1>Contact</h1>
+//     ) 
+// }
